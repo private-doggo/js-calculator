4161d60610150f81e5e66a580e4e100b0a236042
private-doggo <honor20ryhor@gmail.com>
2021-08-12 15:46:43 +0300

    feat: implemented custom calculating mathematical expressions

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -2,15 +2,25 @@ const numpad = document.querySelector('.numpad');
 const input = document.querySelector('.input');
 
 const mathEval = (str) => {
-  // str.replaceAll(/\+-|-\+/, '-');
-  // str = str.split('');
-  // while (str.includes('×')) {
-  //   const i = str.indexOf('×');
-  //   str.splice(i - 1, 3, str[i - 1] * str[i + 1]);
-  // }
-  //
-  // if (str.length === 1) return str[0];
-  // else return undefined;
+  if (!/(?!.*([/+])\1)^-?\d+((?:\/?[+-]{1,2}|[/÷×])\d+)+$/gm.test(str)) return undefined;
+  str = str.replace(/\+-|-\+/g, '-').replace(/--/g, '+').split(/(\+|-|×|÷)/);
+  while (str.includes('÷')) {
+    const i = str.indexOf('÷');
+    str.splice(i - 1, 3, str[i - 1] / str[i + 1]);
+  }
+  while (str.includes('×')) {
+    const i = str.indexOf('×');
+    str.splice(i - 1, 3, str[i - 1] * str[i + 1]);
+  }
+  while (str.includes('-')) {
+    const i = str.indexOf('-');
+    str.splice(i - 1, 3, str[i - 1] - str[i + 1]);
+  }
+  while (str.includes('+')) {
+    const i = str.indexOf('+');
+    str.splice(i - 1, 3, +str[i - 1] + +str[i + 1]);
+  }
+  return str[0];
 }
 
 const keyPress = (e) => {
@@ -28,7 +38,9 @@ const keyPress = (e) => {
       break;
     }
     case 'equals-key': {
-      input.innerHTML = mathEval(input.innerHTML) || 'Error'; // чтобы не было пустого ввода и ошибок
+      const answer = mathEval(input.innerHTML);
+      if (answer !== undefined) input.innerHTML = answer;
+      else input.innerHTML = 'Error';
       break;
     }
   }
