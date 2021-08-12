0417df6579e17d9c8b290735f70549961919e95e
private-doggo <honor20ryhor@gmail.com>
2021-08-12 12:04:27 +0300

    feat: made calculator unselectable

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,10 +1,23 @@
 const numpad = document.querySelector('.numpad');
 const input = document.querySelector('.input');
 
+const mathEval = (str) => {
+  // str.replaceAll(/\+-|-\+/, '-');
+  // str = str.split('');
+  // while (str.includes('×')) {
+  //   const i = str.indexOf('×');
+  //   str.splice(i - 1, 3, str[i - 1] * str[i + 1]);
+  // }
+  //
+  // if (str.length === 1) return str[0];
+  // else return undefined;
+}
+
 const keyPress = (e) => {
   const key = e.target.closest('.key');
-  if (!key) return false;
-  if (key.hasAttribute('writable')) input.innerHTML += key.innerHTML;
+  if (!key) return undefined;
+
+  if (key.hasAttribute('writable')) input.append(key.innerHTML);
   else switch (key.classList[2]) {
     case 'clear-key': {
       input.innerHTML = '';
@@ -15,12 +28,7 @@ const keyPress = (e) => {
       break;
     }
     case 'equals-key': {
-      try {
-        input.innerHTML = eval(input.innerHTML) || 'Error';
-      }
-      catch {
-        input.innerHTML = 'Error';
-      }
+      input.innerHTML = mathEval(input.innerHTML) || 'Error'; // чтобы не было пустого ввода и ошибок
       break;
     }
   }
